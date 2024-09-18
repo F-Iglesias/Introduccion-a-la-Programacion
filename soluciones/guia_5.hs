600f5eaffaee2a53e18ae17d7fe103a2ae7b429a
Francisco <franviglesias@gmail.com>
2024-09-18 11:03:21 -0300

    Add subsections (1.1, ..., 1.4) to exercise 1 in guia_5.hs

--- soluciones/guia_5.hs
+++ soluciones/guia_5.hs
@@ -1,18 +1,24 @@
 
 -- Ejercicio 1
 
+-- 1.1 
 longitud :: [t]->Integer -- Devuelve la cantidad de elementos de una lista
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
+-- 1.2
 ultimo :: [t]->t -- Devuelve el último elemento de la lista dada
 ultimo [x] = x
 ultimo (x:xs) = ultimo xs
 
+
+-- 1.3
 principio :: [t] -> [t] -- Devuelve la lista sin el último elemento. No está definida para listas vacías
 principio [x] = []
 principio(x:xs) = x:(principio xs) 
 
+
+-- 1.4 
 reverso :: [t] -> [t] -- Da como resultado la lista al revéz
 reverso [] = []
 reverso [x] = [x]
